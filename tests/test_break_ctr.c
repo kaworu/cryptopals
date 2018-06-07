607116c8d5a114675b549fc029b8d6b6b7106f3a
Alexandre Perrin <alex@kaworu.ch>
2018-06-07 23:21:03 +0200

    Set 3 / Challenge 20

--- tests/test_break_ctr.c
+++ tests/test_break_ctr.c
@@ -11,8 +11,9 @@
 #include "test_break_ctr.h"
 
 
+/* Set 3 / Challenge 19 */
 static MunitResult
-test_ctr_fixed_nonce(const MunitParameter *params, void *data)
+test_ctr_fixed_nonce_1(const MunitParameter *params, void *data)
 {
 	struct bytes **plaintexts, **ciphertexts;
 	struct bytes *key = bytes_randomized(aes_128_keylength());
@@ -22,19 +23,26 @@ test_ctr_fixed_nonce(const MunitParameter *params, void *data)
 
 	const size_t count = sizeof(s3c19_plaintexts_base64) /
 		    sizeof(*s3c19_plaintexts_base64);
-	plaintexts  = munit_calloc(count, sizeof(struct bytes *));
-	ciphertexts = munit_calloc(count, sizeof(struct bytes *));
+
+	/* decode the plaintexts and compute the longest length */
+	plaintexts = munit_calloc(count, sizeof(struct bytes *));
 	size_t maxlen = 0;
 	for (size_t i = 0; i < count; i++) {
-		struct bytes *plaintext, *ciphertext;
+		struct bytes *plaintext;
 		plaintext = bytes_from_base64(s3c19_plaintexts_base64[i]);
 		if (plaintext == NULL)
 			munit_error("bytes_from_base64");
 		maxlen = (plaintext->len > maxlen ? plaintext->len : maxlen);
-		ciphertext = aes_128_ctr_encrypt(plaintext, key, nonce);
+		plaintexts[i] = plaintext;
+	}
+
+	/* encrypt the plaintexts to generate the ciphertexts */
+	ciphertexts = munit_calloc(count, sizeof(struct bytes *));
+	for (size_t i = 0; i < count; i++) {
+		struct bytes *ciphertext;
+		ciphertext = aes_128_ctr_encrypt(plaintexts[i], key, nonce);
 		if (ciphertext == NULL)
 			munit_error("aes_128_ctr_encrypt");
-		plaintexts[i]  = plaintext;
 		ciphertexts[i] = ciphertext;
 	}
 
@@ -42,27 +50,19 @@ test_ctr_fixed_nonce(const MunitParameter *params, void *data)
 	munit_assert_not_null(keystream);
 	munit_assert_size(keystream->len, ==, maxlen);
 
-	/* break_ctr_fixed_nonce() successfully cracked about half of the
-	   keystream. For the rest I've looked at the decrypted version of all
-	   version of ciphertext and deduced one byte at a time which was easy.
-	   The end of the keystream is harder to guess because there are less
-	   content to be cracked, I had to cheat only for the very last byte. */
+	/*
+	 * break_ctr_fixed_nonce() successfully cracked most of the keystream.
+	 * For the rest I've looked at the guessed plaintexts and deduced one
+	 * byte at a time which was easy. The end of the keystream is harder to
+	 * guess because there are less content to be cracked, I had to cheat
+	 * only for the very last byte.
+	 */
 	struct {
 		size_t ctidx; /* ciphertext index */
 		size_t idx;   /* character index */
 		uint8_t chr;  /* character at the given (ctidx, idx) position */
 	} override[] = {
-		{ .ctidx = 35, .idx =  9, .chr = 'h' },
-		{ .ctidx = 36, .idx =  7, .chr = 'c' },
-		{ .ctidx = 36, .idx =  5, .chr = 'e' },
-		{ .ctidx = 38, .idx =  0, .chr = 'T' },
-		{ .ctidx = 38, .idx =  1, .chr = 'r' },
-		{ .ctidx = 14, .idx = 26, .chr = 'l' },
-		{ .ctidx =  8, .idx = 28, .chr = 'e' },
-		{ .ctidx = 29, .idx = 29, .chr = 'h' },
-		{ .ctidx =  4, .idx = 30, .chr = 'e' },
-		{ .ctidx = 25, .idx = 31, .chr = 'd' },
-		{ .ctidx = 37, .idx = 32, .chr = ' ' },
+		{ .ctidx =  0, .idx =  0, .chr = 'I' }, /* capitalize */
 		{ .ctidx =  4, .idx = 33, .chr = 'e' },
 		{ .ctidx =  4, .idx = 34, .chr = 'a' },
 		{ .ctidx =  4, .idx = 35, .chr = 'd' },
@@ -81,20 +81,89 @@ test_ctr_fixed_nonce(const MunitParameter *params, void *data)
 	/* XOR each ciphertext with the keystream and verify its value against
 	   the known plaintext */
 	for (size_t i = 0; i < count; i++) {
-		struct bytes *recovered = bytes_dup(ciphertexts[i]);
-		if (recovered == NULL)
-			munit_error("bytes_dup");
-		struct bytes *key = bytes_slice(keystream, 0, recovered->len);
-		if (key == NULL)
+		struct bytes *recovered = ciphertexts[i];
+		struct bytes *mask = bytes_slice(keystream, 0, recovered->len);
+		if (mask == NULL)
 			munit_error("bytes_slice");
-		if (bytes_xor(recovered, key) != 0)
+		if (bytes_xor(recovered, mask) != 0)
 			munit_error("bytes_xor");
 		struct bytes *expected = plaintexts[i];
 		munit_assert_size(recovered->len, ==, expected->len);
 		munit_assert_memory_equal(recovered->len, recovered->data,
 			    expected->data);
-		bytes_free(key);
-		bytes_free(recovered);
+		bytes_free(mask);
+	}
+
+	bytes_free(keystream);
+	for (size_t i = 0; i < count; i++)
+		bytes_free(ciphertexts[i]);
+	free(ciphertexts);
+	for (size_t i = 0; i < count; i++)
+		bytes_free(plaintexts[i]);
+	free(plaintexts);
+	bytes_free(key);
+
+	return (MUNIT_OK);
+}
+
+
+/* Set 3 / Challenge 20 */
+static MunitResult
+test_ctr_fixed_nonce_2(const MunitParameter *params, void *data)
+{
+	struct bytes **plaintexts, **ciphertexts;
+	struct bytes *key = bytes_randomized(aes_128_keylength());
+	if (key == NULL)
+		munit_error("bytes_randomized");
+	const uint64_t nonce = 0x0;
+
+	const size_t count = sizeof(s3c20_plaintexts_base64) /
+		    sizeof(*s3c20_plaintexts_base64);
+
+	/* decode the plaintexts and compute the shortest length */
+	plaintexts = munit_calloc(count, sizeof(struct bytes *));
+	size_t minlen = 0;
+	for (size_t i = 0; i < count; i++) {
+		struct bytes *plaintext;
+		plaintext = bytes_from_base64(s3c20_plaintexts_base64[i]);
+		if (plaintext == NULL)
+			munit_error("bytes_from_base64");
+		minlen = (i == 0 || plaintext->len < minlen ?
+			    plaintext->len : minlen);
+		plaintexts[i] = plaintext;
+	}
+
+	/* encrypt the plaintexts to generate the ciphertexts */
+	ciphertexts = munit_calloc(count, sizeof(struct bytes *));
+	for (size_t i = 0; i < count; i++) {
+		struct bytes *plaintext, *ciphertext;
+		/* trim the plaintext to minlen */
+		plaintext = bytes_slice(plaintexts[i], 0, minlen);
+		if (plaintext == NULL)
+			munit_error("bytes_slice");
+		bytes_free(plaintexts[i]);
+		plaintexts[i] = plaintext;
+		/* encrypt the trimmed version of the plaintext */
+		ciphertext = aes_128_ctr_encrypt(plaintext, key, nonce);
+		if (ciphertext == NULL)
+			munit_error("aes_128_ctr_encrypt");
+		ciphertexts[i] = ciphertext;
+	}
+
+	struct bytes *keystream = break_ctr_fixed_nonce(ciphertexts, count);
+	munit_assert_not_null(keystream);
+	munit_assert_size(keystream->len, ==, minlen);
+
+	/* XOR each ciphertext with the keystream and verify its value against
+	   the known plaintext */
+	for (size_t i = 0; i < count; i++) {
+		struct bytes *recovered = ciphertexts[i];
+		if (bytes_xor(recovered, keystream) != 0)
+			munit_error("bytes_xor");
+		const struct bytes *expected = plaintexts[i];
+		munit_assert_size(recovered->len, ==, expected->len);
+		munit_assert_memory_equal(recovered->len, recovered->data,
+			    expected->data);
 	}
 
 	bytes_free(keystream);
@@ -112,7 +181,8 @@ test_ctr_fixed_nonce(const MunitParameter *params, void *data)
 
 /* The test suite. */
 MunitTest test_break_ctr_suite_tests[] = {
-	{ "ctr_fixed_nonce", test_ctr_fixed_nonce, srand_reset, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+	{ "ctr_fixed_nonce-1", test_ctr_fixed_nonce_1, srand_reset, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+	{ "ctr_fixed_nonce-2", test_ctr_fixed_nonce_2, srand_reset, NULL, MUNIT_TEST_OPTION_NONE, NULL },
 	{
 		.name       = NULL,
 		.test       = NULL,
